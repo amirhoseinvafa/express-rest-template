8c5e4794551bbab019988d5e274c59cc35961fdc
vafa <amirhosein.v1994@gmail.com>
2025-07-31 01:58:12 +0330

    init: inital commit

---
+++ controllers/task.js
@@ -0,0 +1,197 @@
+import Task from "../models/task.js";
+import DB from "../models/db.js";
+
+export default class TaskController {
+    static getTasks(req, res) {
+        let page = 1,
+            limit = 4,
+            finished = undefined,
+            search = "";
+
+        if (req.query.page > 0) {
+            page = parseInt(req.query.page);
+        }
+        if (req.query.limit > 0) {
+            limit = parseInt(req.query.limit);
+        }
+        if (req.query.search) {
+            search = req.query.search;
+        }
+        if (req.query.finished === "true" || req.query.finished === "false") {
+            finished = req.query.finished === "true" ? true : false;
+        }
+
+        try {
+            let tasks = Task.getAllTasks();
+            tasks = tasks.filter((item) => item.title.includes(search));
+            if (finished !== undefined) {
+                tasks = tasks.filter((item) => item.completed === finished);
+            }
+
+            const totalTasks = tasks.length;
+
+            const start = (page - 1) * limit;
+            tasks = tasks.slice(start, start + limit);
+
+            res.json({
+                success: true,
+                body: tasks,
+                message: "All tasks fetched",
+                totalTasks,
+            });
+        } catch (e) {
+            res.status(500).json({
+                success: false,
+                body: null,
+                message: "Internal Server Error",
+            });
+        }
+    }
+
+    static getTaskById(req, res) {
+        try {
+            const task = Task.getTaskById(req.params.id);
+            if (task) {
+                res.json({
+                    success: true,
+                    body: task,
+                    message: "The task fetched successfully",
+                });
+            } else {
+                res.status(404).json({
+                    success: false,
+                    body: null,
+                    message: "Task not found",
+                });
+            }
+        } catch (e) {
+            res.status(500).json({
+                success: false,
+                body: null,
+                message: "Internal Server Error",
+            });
+        }
+    }
+
+    static addTask(req, res) {
+        if (req.body.title) {
+            const title = req.body.title;
+            const completed = req.body.completed ? true : false;
+
+            if (title.length < 3) {
+                return res.status(400).json({
+                    success: false,
+                    body: null,
+                    message: "Title must contains 3 characters or more",
+                });
+            } else if (Task.getTaskByTitle(title)) {
+                return res.status(409).json({
+                    success: false,
+                    body: null,
+                    message: "A task already exists with this title",
+                });
+            }
+
+            try {
+                const task = new Task(title, completed);
+                task.save();
+                res.status(201).json({
+                    success: true,
+                    body: task,
+                    message: "Task created",
+                });
+            } catch (e) {
+                res.status(500).json({
+                    success: false,
+                    body: null,
+                    message: "Internal Server Error",
+                });
+            }
+        } else {
+            res.status(400).json({
+                success: false,
+                body: null,
+                message: "Please send new task title",
+            });
+        }
+    }
+
+    static updateTask(req, res) {
+        if (req.body.title && req.body.completed !== undefined) {
+            const { title, completed } = req.body;
+
+            if (title.length < 3) {
+                return res.status(400).json({
+                    success: false,
+                    body: null,
+                    message: "Title must contains 3 characters or more",
+                });
+            }
+
+            let task = Task.getTaskByTitle(title);
+            if (task && task.id != req.params.id) {
+                return res.status(409).json({
+                    success: false,
+                    body: null,
+                    message: "A task already exists with this title",
+                });
+            }
+
+            task = Task.getTaskById(req.params.id);
+            if (task) {
+                try {
+                    task.title = title;
+                    task.completed = completed;
+                    task.save();
+                    res.json({
+                        success: true,
+                        body: null,
+                        message: "Task updated",
+                    });
+                } catch (e) {
+                    res.status(500).json({
+                        success: false,
+                        body: null,
+                        message: "Internal Server Error",
+                    });
+                }
+            } else {
+                res.status(404).json({
+                    success: false,
+                    body: null,
+                    message: "Task not found",
+                });
+            }
+        } else {
+            res.status(400).json({
+                success: false,
+                body: null,
+                message: "Please provide 'title' and 'completed'",
+            });
+        }
+    }
+
+    static deleteTask(req, res) {
+        try {
+            if (DB.deleteTask(req.params.id)) {
+                res.json({
+                    success: true,
+                    body: null,
+                    message: "The task deleted",
+                });
+            } else {
+                res.status(404).json({
+                    success: false,
+                    body: null,
+                    message: "Task not found",
+                });
+            }
+        } catch (e) {
+            res.status(500).json({
+                success: false,
+                body: null,
+                message: "Internal Server Error",
+            });
+        }
+    }
+}
